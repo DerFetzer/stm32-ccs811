0f569349a6e2d907b0cbeb7104dc540730f78156
DerFetzer <kontakt@der-fetzer.de>
2020-07-09 23:16:19 +0200

    Migrate to STM32 BluePill

--- src/main.rs
+++ src/main.rs
@@ -6,7 +6,6 @@ use rtic::app;
 
 
 use panic_semihosting as _;
-use cortex_m_semihosting::hprintln;
 
 use embedded_hal::digital::v2::OutputPin;
 use embedded_hal::prelude::*;
@@ -18,30 +17,37 @@ use stm32f1xx_hal::{
     delay::Delay,
     i2c::{BlockingI2c, DutyCycle, Mode},
     gpio::*,
-    serial::{Config, Serial, Event as SerialEvent},
+    usb::{Peripheral, UsbBus, UsbBusType},
     timer::{Event as TimerEvent, Timer},
 };
 use asm_delay::{AsmDelay, bitrate};
-use core::fmt::Write;
 
 use heapless::Vec;
 use heapless::consts::*;
 use core::convert::TryInto;
 
+use usb_device::bus;
+use usb_device::prelude::*;
+use usbd_serial::{SerialPort, USB_CLASS_CDC};
+use heapless::String;
+use ufmt::uwriteln;
+
 #[app(device = stm32f1xx_hal::pac, peripherals = true)]
 const APP: () = {
     struct Resources {
-        serial_tx: stm32f1xx_hal::serial::Tx<stm32f1::stm32f100::USART1>,
-        serial_rx: stm32f1xx_hal::serial::Rx<stm32f1::stm32f100::USART1>,
+        usb_dev: usb_device::device::UsbDevice<'static, stm32_usbd::bus::UsbBus<stm32f1xx_hal::usb::Peripheral>>,
+        serial: SerialPort<'static, stm32_usbd::bus::UsbBus<stm32f1xx_hal::usb::Peripheral>>,
         serial_rx_buf: Vec<u8, U128>,
-        baseline_timer: stm32f1xx_hal::timer::CountDownTimer<stm32f1::stm32f100::TIM1>,
-        ccs811: Ccs811<stm32f1xx_hal::i2c::BlockingI2c<stm32f1::stm32f100::I2C1, (stm32f1xx_hal::gpio::gpiob::PB8<stm32f1xx_hal::gpio::Alternate<stm32f1xx_hal::gpio::OpenDrain>>, stm32f1xx_hal::gpio::gpiob::PB9<stm32f1xx_hal::gpio::Alternate<stm32f1xx_hal::gpio::OpenDrain>>)>, stm32f1xx_hal::gpio::gpiob::PB7<stm32f1xx_hal::gpio::Output<stm32f1xx_hal::gpio::PushPull>>, asm_delay::AsmDelay, embedded_ccs811::mode::App>,
+        baseline_timer: stm32f1xx_hal::timer::CountDownTimer<stm32f1::stm32f103::TIM1>,
+        ccs811: Ccs811<stm32f1xx_hal::i2c::BlockingI2c<stm32f1::stm32f103::I2C1, (stm32f1xx_hal::gpio::gpiob::PB8<stm32f1xx_hal::gpio::Alternate<stm32f1xx_hal::gpio::OpenDrain>>, stm32f1xx_hal::gpio::gpiob::PB9<stm32f1xx_hal::gpio::Alternate<stm32f1xx_hal::gpio::OpenDrain>>)>, stm32f1xx_hal::gpio::gpiob::PB7<stm32f1xx_hal::gpio::Output<stm32f1xx_hal::gpio::PushPull>>, asm_delay::AsmDelay, embedded_ccs811::mode::App>,
         int_pin: gpiob::PB6<stm32f1xx_hal::gpio::Input<stm32f1xx_hal::gpio::PullUp>>,
         algo_res: AlgorithmResult,
     }
 
     #[init]
     fn init(cx: init::Context) -> init::LateResources {
+        static mut USB_BUS: Option<bus::UsbBusAllocator<UsbBusType>> = None;
+
         // Take ownership over the raw flash and rcc devices and convert them into the corresponding
         // HAL structs
         let mut flash = cx.device.FLASH.constrain();
@@ -53,31 +59,72 @@ const APP: () = {
         let clocks = rcc
             .cfgr
             .use_hse(8.mhz())
-            .sysclk(24.mhz())
+            .sysclk(48.mhz())
             .pclk1(24.mhz())
             .freeze(&mut flash.acr);
+
+        assert!(clocks.usbclk_valid());
+
+        let mut delay = Delay::new(cx.core.SYST, clocks);
+        let mut asm_delay = AsmDelay::new(bitrate::U32BitrateExt::mhz(77));
 
         let mut afio = cx.device.AFIO.constrain(&mut rcc.apb2);
 
         let mut gpioa = cx.device.GPIOA.split(&mut rcc.apb2);
         let mut gpiob = cx.device.GPIOB.split(&mut rcc.apb2);
 
-        // USART1
-        let tx = gpioa.pa9.into_alternate_push_pull(&mut gpioa.crh);
-        let rx = gpioa.pa10;
+        /*let mut debug_pin =  gpioa.pa10.into_push_pull_output(&mut gpioa.crh);
 
-        let mut serial = Serial::usart1(
-            cx.device.USART1,
-            (tx, rx),
-            &mut afio.mapr,
-            Config::default(),
-            clocks,
-            &mut rcc.apb2,
-        );
+        debug_pin.set_low().unwrap();
+        delay.delay_ms(5_u16);
+        debug_pin.set_high().unwrap();
+        delay.delay_ms(5_u16);
+        debug_pin.set_low().unwrap();
+        delay.delay_ms(5_u16);
+        debug_pin.set_high().unwrap();
+        delay.delay_ms(5_u16);
+        debug_pin.set_low().unwrap();
 
-        serial.listen(SerialEvent::Rxne);
+        delay.delay_ms(10_u16);
 
-        let (tx, rx) = serial.split();
+        debug_pin.set_low().unwrap();
+        asm_delay.delay_ms(5_u16);
+        debug_pin.set_high().unwrap();
+        asm_delay.delay_ms(5_u16);
+        debug_pin.set_low().unwrap();
+        asm_delay.delay_ms(5_u16);
+        debug_pin.set_high().unwrap();
+        asm_delay.delay_ms(5_u16);
+        debug_pin.set_low().unwrap();*/
+
+        // USB Serial
+
+        // BluePill board has a pull-up resistor on the D+ line.
+        // Pull the D+ pin down to send a RESET condition to the USB bus.
+        // This forced reset is needed only for development, without it host
+        // will not reset your device when you upload new firmware.
+        let mut usb_dp = gpioa.pa12.into_push_pull_output(&mut gpioa.crh);
+        usb_dp.set_low().unwrap();
+        delay.delay_ms(100_u16);
+        let usb_dm = gpioa.pa11;
+        let usb_dp = usb_dp.into_floating_input(&mut gpioa.crh);
+
+        let usb = Peripheral {
+            usb: cx.device.USB,
+            pin_dm: usb_dm,
+            pin_dp: usb_dp,
+        };
+
+        *USB_BUS = Some(UsbBus::new(usb));
+
+        let serial = SerialPort::new(USB_BUS.as_ref().unwrap());
+
+        let usb_dev = UsbDeviceBuilder::new(USB_BUS.as_ref().unwrap(), UsbVidPid(0x16c0, 0x27dd))
+            .manufacturer("Fake company")
+            .product("Serial port")
+            .serial_number("TEST")
+            .device_class(USB_CLASS_CDC)
+            .build();
 
         let mut int_pin = gpiob.pb6.into_pull_up_input(&mut gpiob.crl);
 
@@ -112,9 +159,6 @@ const APP: () = {
             Timer::tim1(cx.device.TIM1, &clocks, &mut rcc.apb2).start_count_down(1.hz());
         baseline_timer.listen(TimerEvent::Update);
 
-        let mut delay = Delay::new(cx.core.SYST, clocks);
-        let asm_delay = AsmDelay::new(bitrate::U32BitrateExt::mhz(24));
-
         let address = SlaveAddr::Alternative(true);
         let mut ccs811= Ccs811::new(i2c, address, nwake, asm_delay);
 
@@ -122,11 +166,6 @@ const APP: () = {
         delay.delay_ms(3_u16);
         let mut ccs811 = ccs811.start_application().ok().unwrap();
         delay.delay_ms(2_u8);
-        let temperature_c = 25.0;
-        let relative_humidity_perc = 60.0;
-        ccs811
-            .set_environment(temperature_c, relative_humidity_perc)
-            .unwrap();
         ccs811.set_interrupt_mode(InterruptMode::OnDataReady).unwrap();
         ccs811.set_mode(MeasurementMode::LowPowerPulseHeating60s).unwrap();
 
@@ -138,8 +177,8 @@ const APP: () = {
         */
 
         init::LateResources {
-            serial_tx: tx,
-            serial_rx: rx,
+            usb_dev,
+            serial,
             serial_rx_buf: Vec::<u8, U128>::new(),
             baseline_timer,
             ccs811,
@@ -153,8 +192,8 @@ const APP: () = {
         }
     }
 
-    #[task(binds = EXTI9_5, priority = 2, resources = [int_pin, ccs811, algo_res, serial_tx])]
-    fn ext_int(cx: ext_int::Context) {
+    #[task(binds = EXTI9_5, priority = 2, resources = [int_pin, ccs811, algo_res, serial])]
+    fn ext_int(mut cx: ext_int::Context) {
         if cx.resources.int_pin.check_interrupt() {
             let default = AlgorithmResult {
                 eco2: 9999,
@@ -163,21 +202,29 @@ const APP: () = {
                 raw_voltage: 9999,
             };
 
-            *cx.resources.algo_res = cx.resources.ccs811.data().unwrap_or(default);
-            writeln!(cx.resources.serial_tx, "eCO2: {}, eTVOC: {}, raw_current: {}, raw_voltage: {}\r",
+            *cx.resources.algo_res = cx.resources.ccs811.lock(|ccs811| {
+                ccs811.data().unwrap_or(default)
+            });
+            let mut s: String<U128> = String::new();
+            uwriteln!(s, "eCO2: {}, eTVOC: {}, raw_current: {}, raw_voltage: {}\r",
                      cx.resources.algo_res.eco2,
                      cx.resources.algo_res.etvoc,
                      cx.resources.algo_res.raw_current,
                      cx.resources.algo_res.raw_voltage)
                 .unwrap();
-            // hprintln!("eCO2: {}, eTVOC: {}", cx.resources.algo_res.eco2, cx.resources.algo_res.etvoc).unwrap();
+
+            cx.resources.serial.lock(|serial| {
+                if let Ok(_) = serial.flush() {
+                    serial.write(s.as_bytes()).unwrap();
+                }
+            });
 
             // if we don't clear this bit, the ISR would trigger indefinitely
             cx.resources.int_pin.clear_interrupt_pending_bit();
         }
     }
 
-    #[task(binds = TIM1_UP_TIM16, resources = [ccs811, serial_tx, baseline_timer])]
+    #[task(binds = TIM1_UP, resources = [ccs811, serial, baseline_timer])]
     fn tx_baseline(mut cx: tx_baseline::Context) {
         static mut COUNT: u16 = 0;
 
@@ -186,8 +233,13 @@ const APP: () = {
                 ccs811.baseline().unwrap_or([0; 2])
             });
 
-            cx.resources.serial_tx.lock(|serial_tx| {
-                writeln!(serial_tx, "baseline: [{},{}]\r", baseline[0], baseline[1]).unwrap();
+            let mut s: String<U64> = String::new();
+            uwriteln!(s, "baseline: [{},{}]\r", baseline[0], baseline[1]).unwrap();
+
+            cx.resources.serial.lock(|serial| {
+                if let Ok(_) = serial.flush() {
+                    serial.write(s.as_bytes()).unwrap();
+                }
             });
             *COUNT = 0;
         } else {
@@ -197,82 +249,14 @@ const APP: () = {
         cx.resources.baseline_timer.clear_update_interrupt_flag();
     }
 
-    #[task(binds = USART1, resources = [ccs811, serial_tx, serial_rx, serial_rx_buf])]
-    fn rx_usart(mut cx: rx_usart::Context) {
-        let buf: &mut Vec<u8, U128> = cx.resources.serial_rx_buf;
-        match cx.resources.serial_rx.read() {
-            Ok(b) if b == 13 => {
-                if !buf.is_empty() {
-                    let res= match buf[0] {
-                        0x62 => {
-                            match buf.len() {
-                                3 => cx.resources.ccs811.lock(|ccs811| {
-                                    match ccs811.set_baseline(buf[1..3].try_into().unwrap()) {
-                                        Ok(_) => Ok(()),
-                                        Err(_) => Err("Could not set baseline!")
-                                    }
-                                }),
-                                _ => Err("Command too short!")
-                            }
-                        }
-                        0x65 => {
-                            match buf.len() {
-                                9 => {
-                                    let hum = f32::from_be_bytes(buf[1..5].try_into().unwrap());
-                                    let temp = f32::from_be_bytes(buf[5..9].try_into().unwrap());
-                                    cx.resources.ccs811.lock(|ccs811| {
-                                        match ccs811.set_environment(hum, temp) {
-                                            Ok(_) => Ok(()),
-                                            Err(_) => Err("Could not set environment!")
-                                        }
-                                    })
-                                },
-                                _ => Err("Command too short!")
-                            }
-                        }
-                        0x6D => {
-                            match buf.len() {
-                                2 => {
-                                    let mode = match buf[1] {
-                                        0x30 => Ok(MeasurementMode::Idle),
-                                        0x31 => Ok(MeasurementMode::ConstantPower1s),
-                                        0x32 => Ok(MeasurementMode::PulseHeating10s),
-                                        0x33 => Ok(MeasurementMode::LowPowerPulseHeating60s),
-                                        0x34 => Ok(MeasurementMode::ConstantPower250ms),
-                                        _ => Err("Invalid measurement mode!")
-                                    };
+    #[task(binds = USB_HP_CAN_TX, priority = 3, resources = [usb_dev, serial, serial_rx_buf, ccs811])]
+    fn usb_tx(mut cx: usb_tx::Context) {
+        usb_poll(&mut cx.resources.usb_dev, &mut cx.resources.serial, &mut cx.resources.serial_rx_buf, &mut cx.resources.ccs811);
+    }
 
-                                    match mode {
-                                        Ok(m) => {
-                                            cx.resources.ccs811.lock(|ccs811| {
-                                                match ccs811.set_mode(m) {
-                                                    Ok(_) => Ok(()),
-                                                    Err(_) => Err("Could not set mode!")
-                                                }
-                                            })
-                                        }
-                                        Err(e) => Err(e)
-                                    }
-                                },
-                                _ => Err("Command too short!")
-                            }
-                        }
-                        _ => Err("Invalid command!")
-                    };
-                    cx.resources.serial_tx.lock(|serial_tx| {
-                        match res {
-                            Ok(_) => writeln!(serial_tx, "OK\r").unwrap(),
-                            Err(e) => writeln!(serial_tx, "ERR: {}\r", e).unwrap(),
-                        };
-                    });
-                    buf.clear();
-                };
-            }
-            Ok(b) => if let Err(_) = buf.push(b) {
-                buf.clear()
-            }
-            _ => {}
-        };
+    #[task(binds = USB_LP_CAN_RX0, priority = 3, resources = [usb_dev, serial, serial_rx_buf, ccs811])]
+    fn usb_rx0(mut cx: usb_rx0::Context) {
+        usb_poll(&mut cx.resources.usb_dev, &mut cx.resources.serial, &mut cx.resources.serial_rx_buf, &mut cx.resources.ccs811);
     }
 
     extern "C" {
@@ -281,3 +265,95 @@ const APP: () = {
         fn EXTI3();
     }
 };
+
+fn usb_poll<B: bus::UsbBus>(
+    usb_dev: &mut UsbDevice<'static, B>,
+    serial: &mut SerialPort<'static, B>,
+    buf: &mut Vec<u8, U128>,
+    ccs811: &mut Ccs811<stm32f1xx_hal::i2c::BlockingI2c<stm32f1::stm32f103::I2C1, (stm32f1xx_hal::gpio::gpiob::PB8<stm32f1xx_hal::gpio::Alternate<stm32f1xx_hal::gpio::OpenDrain>>, stm32f1xx_hal::gpio::gpiob::PB9<stm32f1xx_hal::gpio::Alternate<stm32f1xx_hal::gpio::OpenDrain>>)>, stm32f1xx_hal::gpio::gpiob::PB7<stm32f1xx_hal::gpio::Output<stm32f1xx_hal::gpio::PushPull>>, asm_delay::AsmDelay, embedded_ccs811::mode::App>,
+) {
+    if !usb_dev.poll(&mut [serial]) {
+        return;
+    }
+
+    let mut read_buf = [0u8; 64];
+
+    match serial.read(&mut read_buf) {
+        Ok(count) if count > 0 => {
+            for c in read_buf[0..count].iter() {
+                match c {
+                    13 => {
+                        if !buf.is_empty() {
+                            let res= match buf[0] {
+                                0x62 => {
+                                    match buf.len() {
+                                        3 => {
+                                            match ccs811.set_baseline(buf[1..3].try_into().unwrap()) {
+                                                Ok(_) => Ok(()),
+                                                Err(_) => Err("Could not set baseline!")
+                                            }
+                                        },
+                                        _ => Err("Command too short!")
+                                    }
+                                }
+                                0x65 => {
+                                    match buf.len() {
+                                        9 => {
+                                            let hum = f32::from_be_bytes(buf[1..5].try_into().unwrap());
+                                            let temp = f32::from_be_bytes(buf[5..9].try_into().unwrap());
+                                            match ccs811.set_environment(hum, temp) {
+                                                Ok(_) => Ok(()),
+                                                Err(_) => Err("Could not set environment!")
+                                            }
+                                        },
+                                        _ => Err("Command too short!")
+                                    }
+                                }
+                                0x6D => {
+                                    match buf.len() {
+                                        2 => {
+                                            let mode = match buf[1] {
+                                                0x30 => Ok(MeasurementMode::Idle),
+                                                0x31 => Ok(MeasurementMode::ConstantPower1s),
+                                                0x32 => Ok(MeasurementMode::PulseHeating10s),
+                                                0x33 => Ok(MeasurementMode::LowPowerPulseHeating60s),
+                                                0x34 => Ok(MeasurementMode::ConstantPower250ms),
+                                                _ => Err("Invalid measurement mode!")
+                                            };
+
+                                            match mode {
+                                                Ok(m) => {
+                                                    match ccs811.set_mode(m) {
+                                                        Ok(_) => Ok(()),
+                                                        Err(_) => Err("Could not set mode!")
+                                                    }
+                                                }
+                                                Err(e) => Err(e)
+                                            }
+                                        },
+                                        _ => Err("Command too short!")
+                                    }
+                                }
+                                _ => Err("Invalid command!")
+                            };
+                            match res {
+                                Ok(_) => serial.write("OK\r\n".as_bytes()).unwrap(),
+                                Err(e) => {
+                                    let mut s: String<U64> = String::new();
+                                    uwriteln!(s, "ERR: {}\r", e).unwrap();
+                                    serial.write(s.as_bytes()).unwrap()
+                                },
+                            };
+                            buf.clear();
+                        };
+                }
+                    b => if let Err(_) = buf.push(*b) {
+                        buf.clear()
+                    }
+                }
+            }
+        }
+        _ => ()
+    }
+}
+
